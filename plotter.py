807441accd70044915de333532c14d3946398d09
Raman Sharma <raman.sharma0096@gmail.com>
2018-10-21 21:42:34 +0200

    Add second file option

--- plotter.py
+++ plotter.py
@@ -2,8 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
-fname = cbook.get_sample_data('C:/Python Projects/graph_plotter/test_plot.csv', asfileobj=False)
+total_alive = cbook.get_sample_data('C:/Java Projects/quality_assurance/evaluate_total_alive.csv', asfileobj=False)
+total_area = cbook.get_sample_data('C:/Java Projects/quality_assurance/evaluate_total_area.csv', asfileobj=False)
 
-plt.plotfile(fname, ('x','hello','world','tree'),delimiter=';',subplots=False)
+option = total_area
+
+plt.plotfile(option, ('time_limit','default','radius','in_range','osm_id','random'),delimiter=';',subplots=False)
+
+plt.xlabel('time t')
+
+if option == total_alive:
+    plt.title('Bayern - Total alive')
+    plt.ylabel('number of centers alive')
+
+else:
+    plt.title('Bayern - Total area')
+    plt.ylabel('total area')
 
 plt.show()
