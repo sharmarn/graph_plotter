7ac39822dcfa1012a8b366100453dc91d81c1ce4
Raman Sharma <raman.sharma0096@gmail.com>
2018-10-25 13:15:56 +0200

    Rename class

--- graph_plotter.py
+++ graph_plotter.py
@@ -7,30 +7,31 @@ total_area = cbook.get_sample_data('C:/Java Projects/quality_assurance/total_are
 total_alive_helper = cbook.get_sample_data('C:/Java Projects/quality_assurance/count_total_alive.csv', asfileobj=False)
 total_area_helper = cbook.get_sample_data('C:/Java Projects/quality_assurance/count_total_area.csv', asfileobj=False)
 
-option = total_alive
-cityName = 'Hamburg'
+option = total_alive_helper
+cityName = 'Berlin'
 
 plt.plotfile(option, ('time_limit','default','radius','in_range','osm_id','random'),delimiter=';',subplots=False)
 
-#plt.yscale('log')
-#plt.yticks(np.arange(10^3, 10^4, 1))
+#plt.yticks(np.arange(0, 10^8, 1000))
 
 plt.xlabel('elimination time')
 
 if option == total_alive:
-    plt.title(cityName + ' - Total alive centers')
+    plt.yscale('log')
+    plt.title('Total alive centers for ' + cityName)
     plt.ylabel('number of centers alive')
 
 elif option == total_area:
-    plt.title(cityName + ' - Total covered area')
+    plt.yscale('log')
+    plt.title('Total covered area for ' + cityName)
     plt.ylabel('total covered area')
 
 elif option == total_alive_helper:
-    plt.title(cityName + ' - MAX Total Alive')
-    plt.ylabel('count')
+    plt.title(cityName + ' - Frequency for Total Alive')
+    plt.ylabel('number of frequency')
 
 else:
-    plt.title(cityName + ' - MAX Total Area')
-    plt.ylabel('count')
+    plt.title(cityName + ' - Frequency for Total Area')
+    plt.ylabel('number of frequency')
 
 plt.show()
