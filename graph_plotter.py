5ff46b6ab49d393406b1307ae72feeadd4e2c1b9
Raman Sharma <raman.sharma0096@gmail.com>
2018-11-22 20:23:48 +0100

    Add new path

--- graph_plotter.py
+++ graph_plotter.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
+#C:/Java Projects/quality_assurance/total_alive.csv
+#C:/Users/Raman Sharma/Documents/Uni/Bachelorarbeit/OutputFiles_Final/Hessen/total_alive.csv
 total_alive = cbook.get_sample_data('C:/Java Projects/quality_assurance/total_alive.csv', asfileobj=False)
 total_area = cbook.get_sample_data('C:/Java Projects/quality_assurance/total_area.csv', asfileobj=False)
-total_alive_helper = cbook.get_sample_data('C:/Java Projects/quality_assurance/count_total_alive.csv', asfileobj=False)
 total_area_helper = cbook.get_sample_data('C:/Java Projects/quality_assurance/count_total_area.csv', asfileobj=False)
 
-option = total_area
-cityName = 'Baden-Württemberg'
+option = total_alive
+cityName = 'Germany'
 
 plt.plotfile(option, ('time_limit','default','radius','in_range','random'),delimiter=';',subplots=False)
 
@@ -22,15 +23,10 @@ if option == total_alive:
 elif option == total_area:
     plt.yscale('log')
     plt.title(cityName + ' - Total Area')
-    plt.ylabel('total covered area by label disks')
-
-elif option == total_alive_helper:
-    plt.title(cityName + ' - Frequency for Total Alive')
-    plt.ylabel('number of frequency')
+    plt.ylabel('total area covered by label disks')
 
 else:
     plt.title(cityName + ' - Frequency for Total Area')
-    plt.ylabel('number of frequency')
+    plt.ylabel('number of highest value')
 
-#plt.yticks(np.arange(10^7, 10^9, 1000))
 plt.show()
