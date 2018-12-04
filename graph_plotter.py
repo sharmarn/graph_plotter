44d33a8cfb41684e8b7acbab63f6bf88fc57c379
Raman Sharma <raman.sharma0096@gmail.com>
2018-12-04 12:47:24 +0100

    Write understandable comments to make functions understandable

--- graph_plotter.py
+++ graph_plotter.py
@@ -2,31 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
-#C:/Java Projects/quality_assurance/total_alive.csv
-#C:/Users/Raman Sharma/Documents/Uni/Bachelorarbeit/OutputFiles_Final/Hessen/total_alive.csv
+# Enter the path to the files
 total_alive = cbook.get_sample_data('C:/Java Projects/quality_assurance/total_alive.csv', asfileobj=False)
 total_area = cbook.get_sample_data('C:/Java Projects/quality_assurance/total_area.csv', asfileobj=False)
-total_area_helper = cbook.get_sample_data('C:/Java Projects/quality_assurance/count_total_area.csv', asfileobj=False)
 
+# Set one option, that are given above
 option = total_alive
-cityName = 'Germany'
 
-plt.plotfile(option, ('time_limit','default','radius','in_range','random'),delimiter=';',subplots=False)
+# Set the name of the region for the headline of the graph
+cityName = 'Hamburg'
+
+# Set the exact heuristic names from the .csv file
+plt.plotfile(option, ('time_limit','default','radius','in_range','random', 'lp_solution'),delimiter=';',subplots=False)
+
+# Set log-scales for y-axis to get a clear graph
+plt.yscale('log')
 
 plt.xlabel('elimination time')
 
 if option == total_alive:
-    plt.yscale('log')
     plt.title(cityName + ' - Total Alive')
     plt.ylabel('number of centers alive')
 
-elif option == total_area:
-    plt.yscale('log')
+else:
     plt.title(cityName + ' - Total Area')
     plt.ylabel('total area covered by label disks')
-
-else:
-    plt.title(cityName + ' - Frequency for Total Area')
-    plt.ylabel('number of highest value')
 
 plt.show()
